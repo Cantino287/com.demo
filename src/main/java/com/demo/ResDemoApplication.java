e3896d10028f572a6829a29832bb5dcbc942a8f7
Cantino287 <cantino287@gmail.com>
2025-05-29 03:50:46 +0630

    Update ResDemoApplication.java

--- src/main/java/com/demo/ResDemoApplication.java
+++ src/main/java/com/demo/ResDemoApplication.java
@@ -2,6 +2,9 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ResDemoApplication {
@@ -9,5 +12,14 @@ public class ResDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ResDemoApplication.class, args);
 	}
-
+	
+	@Bean
+  public WebMvcConfigurer configurer(){
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry reg){
+        reg.addMapping("/**").allowedOrigins("*");
+      }
+    };
+  }
 }
