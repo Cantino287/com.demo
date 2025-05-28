9fce2bc652acd5810a7de597ccea82e6660d8b06
Cantino287 <cantino287@gmail.com>
2025-05-29 01:09:03 +0630

    Update ShopRest.java

--- src/main/java/com/demo/rest/ShopRest.java
+++ src/main/java/com/demo/rest/ShopRest.java
@@ -5,6 +5,7 @@ import com.demo.POJO.Shop;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Map;
