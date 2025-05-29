ed139238e62eb822b883b9f0972c6e1ec2482f12
Cantino287 <cantino287@gmail.com>
2025-05-29 11:56:13 +0630

    Update CorsConfig.java

--- src/main/java/com/demo/config/CorsConfig.java
+++ src/main/java/com/demo/config/CorsConfig.java
@@ -1,28 +1,28 @@
 package com.demo.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                            "http://localhost:5173", // for local dev
-                            "https://frontend-alpha-gilt-12.vercel.app",
-                            "https://qr-rho-ivory.vercel.app",
-                            "https://admin-page-opal-three.vercel.app"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
+// @Configuration
+// public class CorsConfig {
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**")
+//                         .allowedOrigins(
+//                             "http://localhost:5173", // for local dev
+//                             "https://frontend-alpha-gilt-12.vercel.app",
+//                             "https://qr-rho-ivory.vercel.app",
+//                             "https://admin-page-opal-three.vercel.app"
+//                         )
+//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                         .allowedHeaders("*")
+//                         .allowCredentials(true);
+//             }
+//         };
+//     }
+// }
