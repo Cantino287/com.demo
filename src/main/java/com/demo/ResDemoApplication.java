0c3fe856cc84b0f265ac9ca7b197d2a9405149ae
Cantino287 <cantino287@gmail.com>
2025-05-29 07:22:20 +0630

    Update ResDemoApplication.java

--- src/main/java/com/demo/ResDemoApplication.java
+++ src/main/java/com/demo/ResDemoApplication.java
@@ -1,5 +1,5 @@
 package com.demo;
-
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@ComponentScan("com.demo.JWT") // Replace with the actual package of CorsConfig
 public class ResDemoApplication {
 
 	public static void main(String[] args) {
