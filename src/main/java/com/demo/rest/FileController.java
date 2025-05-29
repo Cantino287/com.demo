627ca0d15571d6c137b420d0f76b79bdd8cb56b4
Lin2104 <cantino287@gmail.com>
2025-05-29 14:22:17 +0630

    Fix images

--- src/main/java/com/demo/rest/FileController.java
+++ src/main/java/com/demo/rest/FileController.java
@@ -12,12 +12,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RestController
-@RequestMapping("/images")
+@RequestMapping("/")
 public class FileController {
 
-    private final String BASE_DIRECTORY = "./com.demo/uploads/";
+    private final String BASE_DIRECTORY = "images";
 
-    @GetMapping("/{folder}/{filename:.+}")
+    @GetMapping("/{filename:.+}")
     public ResponseEntity<Resource> getImage(
             @PathVariable String folder,
             @PathVariable String filename) {
